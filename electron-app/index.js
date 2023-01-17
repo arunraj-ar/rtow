0181b61054be84e230f4f72bfd99fb89f422f9f6
arunraj-ar <mail.arunrajar@gmail.com>
2023-01-17 13:44:04 +0530

    rtow-electron-app: #commit-001                                 Features:       Added electron App.                                 Risks:          Executable file yet to be added.                                 Test Procedure: Try running the app using electron.

---
+++ electron-app/index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = (width, height) => {
+  const win = new BrowserWindow({
+    height: height,
+    width: width,
+    title: "rtow",
+    fullscreen: true,
+    titleBarStyle: "hidden",
+    resizable: false,
+    titleBarOverlay: true,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  const { screen } = require("electron");
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width, height } = primaryDisplay.size;
+  createWindow(width, height);
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
